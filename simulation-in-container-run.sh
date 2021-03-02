30910ccd3fca830b5ebe4f520e55357f025b6eef
Guillem Gari <ggari@robotnik.es>
2021-03-02 14:18:13 +0100

    added current functions

--- simulation-in-container-run.sh
+++ simulation-in-container-run.sh
@@ -28,15 +28,18 @@ function tools_check() {
 }
 
 function build_image() {
-    return 0
+    docker container rm --force summit_xl_sim
+    return $?
 }
 
 function check_docker_instance_already_running() {
-    return 0
+    docker container ls -a | sed '1d' | awk '{print $2}' | grep -q ^summit_xl_sim$
+    return $?
 }
 
 function delete_running_docker_instance() {
-    return 0
+    docker container rm --force summit_xl_sim
+    return $?
 }
 
 function allow_screen() {
@@ -48,7 +51,21 @@ function disable_screen() {
 }
 
 function run_simulation() {
-    return 0
+    docker run --gpus all --rm \
+        --env="DISPLAY=$DISPLAY" \
+        --env="QT_X11_NO_MITSHM=1" \
+        --device=/dev/dri \
+        --group-add video \
+        --volume="/tmp/.X11-unix:/tmp/.X11-unix:rw" \
+        --env ROS_MASTER_URI="http://localhost:11311" \
+        --env GAZEBO_MASTER_URI="http://localhost:11345" \
+        --env NVIDIA_VISIBLE_DEVICES=0 \
+        --name summit_xl_sim \
+        summit_xl_sim:latest \
+        bash -c "source /opt/ros/melodic/setup.bash &&
+            source /home/ros/catkin_ws/devel/setup.bash &&
+            roslaunch summit_xl_sim_bringup summit_xl_complete.launch"
+    return $?
 }
 
 function simulation_flow() {
